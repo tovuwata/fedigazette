5f92e537eee6007a931f9421037d886a3d2e37a4
dampuzakura <168283013+dampuzakura@users.noreply.github.com>
2024-08-11 01:25:34 +0900

    🔧 transformer-directivesの設定

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig, presetUno } from 'unocss';
+import { defineConfig, presetUno, transformerDirectives } from 'unocss';
 
 export default defineConfig({
   presets: [presetUno()],
+  transformers: [transformerDirectives()],
 });
