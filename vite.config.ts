dc9d93cc177e905f98a3660ff4e718d548fdc3b5
dampuzakura <168283013+dampuzakura@users.noreply.github.com>
2024-08-11 00:20:08 +0900

    🔧 UnoCSS

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import Icons from 'unplugin-icons/vite';
 import { defineConfig } from 'vite';
 
 import { sveltekit } from '@sveltejs/kit/vite';
+import UnoCSS from '@unocss/svelte-scoped/vite';
 
 export default defineConfig({
   plugins: [
@@ -9,5 +10,6 @@ export default defineConfig({
     Icons({
       compiler: 'svelte',
     }),
+    UnoCSS(),
   ],
 });
