2b37ef65d6d268c7bc0802b379c6c2b8533219a5
dampuzakura <168283013+dampuzakura@users.noreply.github.com>
2024-08-19 18:52:52 +0900

    ✨ お問い合わせ

---
+++ src/routes/contact/Contact.svelte
@@ -0,0 +1,9 @@
+<p class="p">
+  ご不明点などについては、
+  <a href="https://forms.gle/r14xM6YZG5CXxhaLA" target="_blank" class="button"
+    >こちら</a
+  >または
+  <a href="https://tovuwata.bsky.social" target="_blank" class="button"
+    >Bluesky チャット</a
+  >からお問い合わせください。
+</p>
