b92064165920620199ab7c2fb5877ff4b07f69f6
dampuzakura <168283013+dampuzakura@users.noreply.github.com>
2024-08-19 18:52:02 +0900

    ✨ レイアウトの設定

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,35 @@
-<script>
-  export const prerender = true;
+<script lang="ts">
+  import type { SvelteComponent } from 'svelte';
+  import '@unocss/reset/tailwind.css';
+  import '@fontsource/roboto';
+  import '@fontsource/noto-sans-jp';
+  import '@fontsource/noto-emoji';
+  import '../app.scss';
+  import Meta from '$components/Meta.svelte';
+  import Ads from '$components/Ads.svelte';
+
+  let HeaderComponent: typeof SvelteComponent | null = null;
+  let FooterComponent: typeof SvelteComponent | null = null;
+
+  import('$components/Header.svelte').then((module) => {
+    HeaderComponent = module.default as typeof SvelteComponent;
+  });
+
+  import('$components/Footer.svelte').then((module) => {
+    FooterComponent = module.default as typeof SvelteComponent;
+  });
 </script>
 
-<slot />
+<div class="pt-16 dark:bg-gray-900 dark:text-white">
+  {#if HeaderComponent}
+    <svelte:component this={HeaderComponent} />
+  {/if}
+  <div class="min-h-[calc(100vh-4rem)]">
+    <slot />
+    <Ads />
+    <Meta />
+    {#if FooterComponent}
+      <svelte:component this={FooterComponent} />
+    {/if}
+  </div>
+</div>
