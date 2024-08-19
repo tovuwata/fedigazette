200496a1736eb00e29abaa335d5d7557ec5168ef
dampuzakura <168283013+dampuzakura@users.noreply.github.com>
2024-08-19 23:26:32 +0900

    ✨ このサイトについて

--- src/routes/about/About.svelte
+++ src/routes/about/About.svelte
@@ -1,2 +1,14 @@
 <h1 class="h1">このサイトについて</h1>
-<p class="p"></p>
+<p class="p">
+  Tovuwata
+  FediGazetteは、日本語圏ユーザー向けのMisskeyやMastodonなどの分散型SNS（Fediverse）に関する情報を専門に扱うWEBメディアです。<br
+  />本メディアは、さまざまなトピックを取り上げ、分散型SNSの魅力やその活用方法についてお伝えしていくことを目的としています。<br
+  /><br
+  />投稿の頻度は不定期ではありますが、できるだけ継続的に情報を発信し、読者の皆様に有益なコンテンツを提供できるよう努めてまいります。<br
+  />fedimagazine.tokyoのような魅力的な記事を目指しつつも、独自の視点を取り入れた内容をお届けできればと考えております。<br
+  /><br />運営者の段風桜（Twitter: @dampuzakura、Misskey:
+  @dampuzakura@misskey.io、Bluesky:
+  @dampuzakura.bsky.social）は、Tovuwataという新たに立ち上げた開発組織の代表を務めています。<br
+  />私たちの取り組みにご興味を持っていただけましたら、ぜひ応援していただければ幸いです。<br
+  />今後ともどうぞよろしくお願いいたします。
+</p>
