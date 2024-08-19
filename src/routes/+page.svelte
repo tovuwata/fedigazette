0eba40864cfb170c41c559e7b92ecdbc497805f1
dampuzakura <168283013+dampuzakura@users.noreply.github.com>
2024-08-19 18:52:28 +0900

    ✨ トップページ

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { base } from '$app/paths';
+  import About from './about/About.svelte';
+</script>
+
+<main>
+  <section
+    class="flex justify-center items-center h-[calc(100vh-16*0.25rem)] text-black bg-gradient-to-rb from-secondary to-tertiary"
+  >
+    <div class="text-center">
+      <h1 class="text-5xl font-bold">
+        <br />
+      </h1>
+      <p class="mt-4 text-2xl"></p>
+    </div>
+  </section>
+
+  <section class="section">
+    <About />
+    <a href="{base}/about" class="mt-6 button">もっと見る</a>
+  </section>
+</main>
