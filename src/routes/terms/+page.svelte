f2efa3029bcf31702fd41524c520707250e8fe93
dampuzakura <168283013+dampuzakura@users.noreply.github.com>
2024-08-19 18:54:32 +0900

    ✨ 利用規約

---
+++ src/routes/terms/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import Terms from './Terms.svelte';
+</script>
+
+<section class="section">
+  <Terms />
+</section>
---
+++ src/routes/terms/Terms.svelte
@@ -0,0 +1,8 @@
+<h1 class="h1">利用規約</h1>
+<p class="p">
+  <a
+    href="https://tovuwata.github.io/terms/terms"
+    target="_blank"
+    class="button">こちら</a
+  >を御覧ください。
+</p>
