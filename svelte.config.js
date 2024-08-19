4fc9c9a27493d81a03094023c892802aceaf1c82
dampuzakura <168283013+dampuzakura@users.noreply.github.com>
2024-08-19 18:48:52 +0900

    🔧 Svelteの設定

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,33 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import UnoCSS from '@unocss/svelte-scoped/preprocess';
+import { mdsvex } from 'mdsvex';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const newsDir = path.resolve(__dirname, './src/routes/news/posts');
+const newsSvxFiles = fs
+  .readdirSync(newsDir)
+  .filter((file) => file.endsWith('.svx'));
+
+const newsEntries = newsSvxFiles.map(
+  (file) => `/news/view/${file.replace('.svx', '')}`
+);
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: [vitePreprocess(), UnoCSS()],
+  extensions: ['.svelte', '.svx'],
+  preprocess: [
+    vitePreprocess(),
+    mdsvex({
+      extension: '.svx',
+    }),
+    UnoCSS(),
+  ],
 
   kit: {
     adapter: adapter({
@@ -14,6 +37,27 @@ const config = {
       precompress: false,
       strict: true,
     }),
+    paths: {
+      base: '',
+    },
+    alias: {
+      '$components': 'src/components'
+    },
+    prerender: {
+      entries: [
+        '/',
+        '/about',
+        '/contact',
+        '/donate',
+        '/news',
+        '/news/view/_test',
+        '/policy',
+        '/privacy',
+        '/sponsor',
+        '/terms',
+        '/news/rss.xml',
+      ].concat(newsEntries),
+    },
   },
 };
 
